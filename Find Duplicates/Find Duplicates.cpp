be24a293fd735983fc3518ad145355e93db7558c
maher-hanna <maher.hn802@gmail.com>
2020-06-01 18:45:32 +0300

    Print a list of matched files if found

--- Find Duplicates/Find Duplicates.cpp
+++ Find Duplicates/Find Duplicates.cpp
@@ -2,22 +2,24 @@
 #include <filesystem>
 #include <vector>
 #include <algorithm>
+#include <iterator>
+#include <fstream>
 
 using namespace std;
 namespace fs = std::filesystem;
 
 
 //Functions:
-void printProgramUsage();
-vector<string> directoryFiles(const fs::path & path);
-void print(vector<string> vec);
+vector<string> directoryFiles(const fs::path& path);
 vector<string> findMatches(vector<string> filesList);
-bool filesMatches(string firstFilePath, string secondFilePath);
+bool filesIdentical(string firstFilePath, string secondFilePath);
+void printProgramUsage();
+void printList(const vector<string>& vec);
 
 //////
 
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
 	if (argc < 2)
 	{
@@ -28,12 +30,136 @@ int main(int argc, char * argv[])
 	{
 		string directory = argv[1];
 		filesystem::path path(directory);
-        auto filesList = directoryFiles(path);
-        print(findMatches(filesList));
+
+		if(!fs::exists(path))
+		{
+			cout << "The given folder doesn't exist\n";
+			return 1;
+		}
+
+		if (!fs::is_directory(path))
+		{
+			cout << "The given path is not folder\n";
+			return 1;
+		}
+
+		auto filesList = directoryFiles(path);
+		auto filesMatched = findMatches(filesList);
+		if (filesMatched.empty())
+		{
+			cout << "There is no matches\n";
+
+		}
+		else
+		{
+			cout << "Found Matches: \n";
+			printList(filesMatched);
+		}
 
 
 	}
 	return 0;
+}
+
+
+
+vector<string> directoryFiles(const fs::path& path)
+{
+	vector<string> result;
+
+	for (const auto& entry : fs::directory_iterator(path))
+	{
+		const auto& currentEntry = entry.path();
+		if (fs::is_directory(currentEntry))
+		{
+			auto currentDirectoryFiles = directoryFiles(currentEntry);
+			result.insert(result.end(), currentDirectoryFiles.begin(), currentDirectoryFiles.end());
+		}
+		else if (fs::is_regular_file(currentEntry))
+		{
+			result.push_back(currentEntry.u8string());
+		}
+
+	}
+	return result;
+
+}
+
+
+
+vector<string> findMatches(vector<string> filesList)
+{
+	//group ech group of identical files together in the list
+	//by separating them from other group by an empty element
+	vector<string> fileMatches;
+
+	//if the folder containes no files or just one file return empty list of matches
+
+	return fileMatches;
+
+	for (auto i = (filesList.size() - 1); i > 0; i--)
+	{
+
+		//add the file to compare only once at the begining of the group of matched files
+		bool currentFileHasMatches = false;
+		bool firstFileAddedToGroup = false;
+
+		for (long fileToCompareIndex = (i - 1); fileToCompareIndex >= 0; fileToCompareIndex--)
+		{
+
+			if (filesIdentical(filesList[i], filesList[fileToCompareIndex]))
+			{
+				currentFileHasMatches = true;
+				if (!firstFileAddedToGroup)
+				{
+					fileMatches.push_back(filesList[i]);
+					firstFileAddedToGroup = true;
+
+				}
+
+				fileMatches.push_back(filesList[fileToCompareIndex]);
+			}
+
+		}
+
+		if (currentFileHasMatches)
+		{
+			fileMatches.push_back("");
+		}
+		currentFileHasMatches = false;
+
+
+	}
+
+	return fileMatches;
+}
+
+bool filesIdentical(string firstFilePath, string secondFilePath)
+{
+	ifstream firstFile(firstFilePath, ios::ate | ios::binary);
+	ifstream secondFile(secondFilePath, ios::ate | ios::binary);
+
+	auto firstFileSize = firstFile.tellg();
+	auto secondFileSize = secondFile.tellg();
+	if (firstFileSize != secondFileSize)
+	{
+		return false;
+	}
+
+	const size_t readBlockSize = 100;
+	char firstContentBuffer[readBlockSize];
+	char secondContentBuffer[readBlockSize];
+	while (!firstFile.eof())
+	{
+		firstFile.read(firstContentBuffer, readBlockSize);
+		secondFile.read(secondContentBuffer, readBlockSize);
+		if (!equal(firstContentBuffer, firstContentBuffer + sizeof(firstContentBuffer), secondContentBuffer))
+		{
+			return false;
+		}
+	}
+	return true;
+
 }
 
 
@@ -47,58 +173,7 @@ void printProgramUsage()
 }
 
 
-vector<string> directoryFiles(const fs::path & path)
+void printList(const vector<string>& vec)
 {
-    vector<string> result;
-    if (fs::is_directory(path))
-    {
-        for (const auto& entry : fs::directory_iterator(path))
-        {
-            const auto& currentEntry = entry.path();
-            if (fs::is_directory(currentEntry))
-            {
-                auto currentDirectoryFiles = directoryFiles(currentEntry);
-                result.insert(result.end(),currentDirectoryFiles.begin(),currentDirectoryFiles.end());
-            }
-            else if (fs::is_regular_file(currentEntry))
-            {
-                result.push_back(currentEntry.u8string());
-            }
-
-        }
-        return result;
-    }
-}
-
-void print(vector<string> vec)
-{
-    copy(vec.begin(), vec.end(), ostream_iterator<string>(cout, "\n"));
-}
-
-vector<string> findMatches(vector<string> filesList)
-{
-    vector<string> fileMatches;
-    for (auto i = (filesList.size() - 1); i > 0; i--)
-    {
-        for (auto fileToCompareIndex = (i - 1); fileToCompareIndex >= 0; fileToCompareIndex--)
-        {
-            if (filesMatches(filesList[i], filesList[fileToCompareIndex]))
-            {
-                fileMatches.push_back(filesList[i]);
-                fileMatches.push_back(filesList[fileToCompareIndex]);
-            }
-
-        }
-        if (!filesList.back().empty())
-        {
-            filesList.push_back("");
-        }
-    }
-
-    return vector<string>();
-}
-
-bool filesMatches(string firstFilePath, string secondFilePath)
-{
-    return false;
+	copy(vec.begin(), vec.end(), ostream_iterator<string>(cout, "\n"));
 }
